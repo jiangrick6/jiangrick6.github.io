98c905be54d62d12cd9d3b6f5ce2a0b013ca3046
Rick Jiang <Rick Jiang>
2024-03-01 22:44:44 -0500

    Assignment 11 starting

--- assignments/11/script.js
+++ assignments/11/script.js
@@ -8,41 +8,62 @@ class Card {
    
     }
 
-    getSection() {
+    get section() {
         const section = document.createElement("section");
+        
         const h3 = document.createElement("h3");
-        h3.innerHTML(this.title);
+        h3.innerHTML = this.team;
+        section.append(h3);
+
         const image = document.createElement("img");
-        image.src = `images/${file}`;
-        section.append(h3,image);
+        image.src = `images/${this.file}`;
+        image.classList.add("image-small");
+        section.append(image);
+        
+        section.onclick = this.expand;
+        section.classList.add("content");
+        
         return section;
     };
 
-    getExpandedSection() {
+    get infoSectionSection() {
+        console.log("test");
         const section = document.createElement("section");
-        section.className = "card-details";
-        section.innerHTML = `
-            <h3>${this.team}</h3>
-            <img src="images/${this.file}" alt="${this.team}">
-            <p>Description: ${this.description}</p>
-            <p>Record: ${this.record}</p>
-            <p>Key Players: ${this.keyplayers}</p>
-            <p>Rank: ${this.rank}</p>
-        `;
-        return section;
-    }
 
-    expandOrClose(e, summarySection) {
-        if (!this.expanded) {
-            const expandedSection = this.getExpandedSection();
-            summarySection.after(expandedSection);
-            this.expanded = true;
-        } else {
-            summarySection.nextElementSibling.remove();
-            this.expanded = false;
-        }
-    }
-}
+        const h3team = document.createElement("h3");
+        h3.innerHTML = this.team;
+        section.append(h3team);
+
+        const pdescription = document.createElement("p");
+        pType.innerHTML = `Description: ${this.description}`;
+        section.append(pdescription);
+
+
+        const buttonClose = document.createElement("button");
+        buttonClose.innerHTML = "X";
+        buttonClose.onclick = this.close;
+        section.append(buttonClose);
+
+        const image = document.createElement("img");
+        image.src = `images/${this.file}`;
+        image.classList.add("image");
+        section.append(image);
+
+        return section;
+    };
+
+    expand() {
+        document.getElementById("card-info").append(this.infoSection);
+        document.getElementById("transparents").classList.remove("hidden");
+        document.getElementById("card-info").classList.remove("hidden");
+    };
+
+    close() {
+        document.getElementById("transparents").classList.add("hidden");
+        document.getElementById("card-info").classList.remove("hidden");
+        document.getElementById("card-info").innerHTML = "";
+    };
+};  
 
 const cards = [];
 cards.push(new Card("sanfran.png","San Francisco 49ers","Overpaid and Elite Team","12-5","Brock Prody and Nick Bosa","1"));
@@ -51,6 +72,5 @@ cards.push(new Card("seattles.png","Seattle Seahawks","Young Team with the Young
 cards.push(new Card("ari.png","Arizona Carinals","Young team but Low Expectations","4-13","Kyler Murray and Budda Baker","4" ));
 
 cards.forEach((card) => {
-    document.getElementById("card-list").append(card.getSection());
-    document.getElementById("main").append(card.getExpandedSection());
+    document.getElementById("card-team").append(card.section);
 });
